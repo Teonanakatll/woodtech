090356cf030c6573d5a40e3e30d69cf2c995f2f8
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-09-04 14:02:10 +0300

    deploy

--- wood/settings.py
+++ wood/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALL_HOST1'), os.getenv('ALL_HOST2')]
 
 SITE_ID = 1
 
